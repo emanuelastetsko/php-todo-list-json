a4a41e03473612a0cb441376082b866fcb27afcc
Emanuela Stetsko <emanuelastetsko.es@gmail.com>
2023-02-28 17:55:42 +0100

    Lista stilizzata con bootstrap

--- index.php
+++ index.php
@@ -10,16 +10,25 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PHP ToDo List JSON</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="./css/style.css">
+
+        <!-- BOOTSTRAP -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     </head>
     <body>
 
         <main>
             <div id="app">
-                <ul>
-                    <li v-for="(todo, index) in todolist" :key="todo.id">
-                        {{ index + 1 }}. {{ todo.task }}
-                    </li>
-                </ul>
+                <div class="my-container">
+                    <h1>To do list</h1>
+                    <ul class="list-group">
+                        <li v-for="(todo, index) in todolist" :key="todo.id" class="list-group-item">
+                            {{ index + 1 }}. {{ todo.task }}
+                        </li>
+                    </ul>
+                </div>
             </div>
         </main>
 
