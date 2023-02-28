5e7922e1725e39851bde05c69beb17f21d0fcb86
Emanuela Stetsko <emanuelastetsko.es@gmail.com>
2023-02-28 18:34:50 +0100

    Stilizzazione finalizzata

--- index.php
+++ index.php
@@ -22,12 +22,19 @@
         <main>
             <div id="app">
                 <div class="my-container">
-                    <h1>To do list</h1>
-                    <ul class="list-group">
-                        <li v-for="(todo, index) in todolist" :key="todo.id" class="list-group-item">
-                            {{ index + 1 }}. {{ todo.task }}
-                        </li>
-                    </ul>
+                    <div class="to-do-list">
+                        <h1>To do list</h1>
+                        <ul class="list-group">
+                            <li v-for="(todo, index) in todolist" :key="todo.id" class="list-group-item">
+                                {{ todo.task }}
+                                <button type="button" class="btn btn-danger">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                                    </svg>
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </main>
